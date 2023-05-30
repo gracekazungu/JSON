c911a8c20aa5e18dbbb2e2a96d73e83e3912adc6
gracekazungu <kazungungrace@gmail.com>
2023-05-30 17:15:04 +0300

    commit

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-const productContainer=document.getElementById("products");
-
-const getProducts=()=>{
-   return fetch('https://dummyjson.com/products')
-   .then(response=>response.json())
-   .then(response=>response)
-   .catch(error=>error.message)
-};
-getProducts();
-
-const displayProducts=async()=>{
-const products=await getProducts();
-console.log(products.products);
-products.products.map(product=>{
-    // let div=document.createElement("div");
-    // div.className=("items");
-    // let img=document.createElement("img");
-    const productElement = document.createElement('div');
-    productElement.id=('product');
-    const imageElement = document.createElement('img');
-    imageElement.src = product.image;
-    imageElement.alt = product.title;
-    productElement.appendChild(imageElement);
-    const titleElement = document.createElement('h3');
-    titleElement.textContent = product.title;
-    productElement.appendChild(titleElement);
-    const priceElement = document.createElement('p');
-    priceElement.textContent = `${product.price}`;
-    productElement.appendChild(priceElement);
-    productContainer.appendChild(productElement);
-  });
-
-}
-displayProducts();
+function getProductCards(data) {
+    const productContainer = document.getElementById("products");
+    data.forEach((product) => {
+    const card=document.createElement("div");
+    card.classList.add("product-card");
+      card.innerHTML = `
+              <img src="${product.thumbnail}" alt="${product.name}" />
+              <h2>${product.title}</h2>
+              <div class='productPrice'>
+              <p>${product.price}</p>
+              <button>ADD TO CART</button>
+              </div>
+          `;
+      productContainer.appendChild(card);
+    });
+  }
+  
+  function getProducts() {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        const returnedData = data.products.slice(0, 8);
+        getProductCards(returnedData);
+      });
+  }
+  
+  getProducts()
